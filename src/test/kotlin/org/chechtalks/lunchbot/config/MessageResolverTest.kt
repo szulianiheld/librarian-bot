d7337227c49dfbe6204803cece531b6fc8d524c0
Cesar Daniel Perez <cesdperez@gmail.com>
2017-06-11 17:30:06 -0300

    fixed MessageResolverTest

--- src/test/kotlin/org/chechtalks/lunchbot/config/MessageResolverTest.kt
+++ src/test/kotlin/org/chechtalks/lunchbot/config/MessageResolverTest.kt
@@ -1,6 +1,7 @@
 package org.chechtalks.lunchbot.config
 
 import org.hamcrest.Matchers.`is`
+import org.hamcrest.Matchers.containsString
 import org.junit.Assert.assertThat
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -10,19 +11,19 @@ import org.springframework.test.context.junit4.SpringRunner
 
 @RunWith(SpringRunner::class)
 @SpringBootTest
-class MessageResolverTest() {
+class MessageResolverTest {
 
     @Autowired
     private lateinit var messages: MessageResolver
 
     @Test
     fun it_resolves_existent_messages() {
-        assertThat(messages.get("lunchbot.response.default"), `is`("No Entendí"));
+        assertThat(messages.get("lunchbot.response.default"), containsString("No Entendí"))
     }
 
     @Test
     fun it_resolves_nonexistent_messages() {
-        assertThat(messages.get("totally.nonexistent.code"), `is`("totally.nonexistent.code"));
+        assertThat(messages.get("totally.nonexistent.code"), `is`("totally.nonexistent.code"))
     }
 
 }
